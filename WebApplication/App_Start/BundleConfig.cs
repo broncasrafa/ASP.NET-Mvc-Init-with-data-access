53661b4aaac6979e0664124eb4150f6053a470d8
Rafael Francisco <broncasrafa@gmail.com>
2020-02-29 09:18:40 -0300

    adding some js files

--- WebApplication/App_Start/BundleConfig.cs
+++ WebApplication/App_Start/BundleConfig.cs
@@ -9,22 +9,30 @@ namespace WebApplication
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Content/assets/scripts/libs/jquery/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                        "~/Content/assets/scripts/libs/jquery.validate/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryajax").Include(
+                        "~/Content/assets/scripts/libs/jquery.unobtrusive-ajax/jquery.unobtrusive-ajax*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Content/assets/scripts/libs/modernizr/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Content/assets/scripts/libs/bootstrap3/bootstrap.js",
+                      "~/Content/assets/scripts/libs/jquery.blockUI/jquery.blockUI.js",
+                      "~/Content/assets/scripts/libs/toastr/toastr.js",
+                      "~/Content/assets/scripts/app/functions.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/assets/css/bootstrap3/bootstrap.css",
+                      "~/Content/assets/css/app/site.css",
+                      "~/Content/assets/css/toastr/toastr.css",
+                      "~/Content/assets/css/jquery.blockUI/jquery.blockUI.css"));
         }
     }
 }
